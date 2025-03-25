d744731d02667ab8176808959b937f7209a00338
Lukas Frias Santos <santos@fs.tum.de>
2025-03-25 14:05:30 +0100

    now testing online

--- functions/registration.ts
+++ functions/registration.ts
@@ -1,7 +1,11 @@
 export const onRequestPost: PagesFunction<Env> = async (context) => {
     var form: FormData = await context.request.formData();
+    const body = {};
+    for (const entry of form.entries()) {
+        body[entry[0]] = entry[1];
+    }
     
-    return new Response(JSON.stringify(form), {
+    return new Response(JSON.stringify(body), {
         status: 200,
     });
 };
