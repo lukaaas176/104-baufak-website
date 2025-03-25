0db99ba3b683a07f7f261931a85494cad73b37d8
Lukas Frias Santos <santos@fs.tum.de>
2025-03-25 14:22:05 +0100

    test files

--- functions/registration.ts
+++ functions/registration.ts
@@ -2,7 +2,13 @@ export const onRequestPost: PagesFunction<Env> = async (context) => {
     var form: FormData = await context.request.formData();
     const body = {};
     for (const entry of form.entries()) {
-        body[entry[0]] = entry[1];
+        if (entry[1] instanceof File) {
+            var file: File = entry[1];
+            var content: Uint8Array = await file.bytes();
+            body[entry[0]] = file.name + ": " + btoa(content.reduce((data, byte) => data + String.fromCharCode(byte), ''))
+        } else {
+            body[entry[0]] = entry[1];
+        }
     }
     
     return new Response(JSON.stringify(body), {
