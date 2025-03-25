48449d79636026525920e91f9d5dc02ba3af7634
Lukas Frias Santos <santos@fs.tum.de>
2025-03-25 17:11:13 +0100

    fix typo

--- functions/registration.ts
+++ functions/registration.ts
@@ -75,7 +75,7 @@ function checkNotEmpty(formData: FormData, validationErrors: Map<string, string>
 
 function checkEmail(formData: FormData, validationErrors: Map<string, string>): string {
     let entry: string = checkNotEmpty(formData, validationErrors, "email", "Die E-Mail");
-    if (!entry.match(/^[\w\-\.]+@([\w-]+\.)+[\w-]{2,}$)/)) {
+    if (!entry.match(/^[\w\-\.]+@([\w-]+\.)+[\w-]{2,}$/)) {
         validationErrors.set("email", "Die E-Mail hat ein ungültiges Format!");
     }
     return entry;
